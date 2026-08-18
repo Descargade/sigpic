1ea3e783fd2d2b108b961638a3fa9b579d758ae1
Aaron Gonzalez <gonzalezlucasaaron@gmail.com>
2026-08-18 12:15:37 -0300

    fix: reduce max-w to 600px for truly vertical viewport (600x650)

--- artifacts/sigpic/src/pages/diagramas.tsx
+++ artifacts/sigpic/src/pages/diagramas.tsx
@@ -816,7 +816,7 @@ function DiagramInner({
       )}
 
       {/* Canvas */}
-      <div ref={wrapperRef} className={`h-[650px] max-w-[700px] border rounded-xl shadow-inner ${bgClass}`}>
+      <div ref={wrapperRef} className={`h-[650px] max-w-[600px] border rounded-xl shadow-inner ${bgClass}`}>
         <ReactFlow
           nodes={nodesWithExpanded}
           edges={visibleEdges}
